a87342e1eb956145857ff4434f5321b49a2ae5ab
hnw511 <yazdan.shirvany@capitalone.com>
2016-12-23 14:37:16 -0500

    Docker

--- src/main/java/com/gcit/dao/BaseDAO.java
+++ src/main/java/com/gcit/dao/BaseDAO.java
@@ -1,21 +1,33 @@
 package com.gcit.dao;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public abstract class BaseDAO {
-	
+	@Value("${spring.datasource.password}")
+	String DBPassword;
+
+	@Value("${spring.datasource.username}")
+	String DBUsername;
+
+	@Value("${spring.datasource.url}")
+	String DBUrl;
+
 	protected Connection getConnection() throws SQLException {
 		
 		try {
+
+			System.out.println("DBPassword = " + DBPassword);
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 		Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/library", 
-				"root", "root");
+				DBUrl,
+				DBUsername, DBPassword);
 		return conn;
 	}
 
